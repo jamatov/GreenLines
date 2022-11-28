65f9681b6e6b3f8b769c891621a347e21a8cefab
jamatov <mirabdullajamatov@gmail.com>
2022-11-28 14:20:52 +0500

    done

--- src/components/AboutHead.js
+++ src/components/AboutHead.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { useState } from 'react'
+import HomeMain from './HomeMain';
+import HomeSlide from './HomeSlide';
 
 export default function AboutHead() {
+  const [navbar, setNavbar] = useState(false);
+
+
+  const changeNavbar = () => {
+    if (window.scrollY >= 30) {
+        setNavbar(true);
+    } else {
+        setNavbar(false)
+    }
+  }
+  window.addEventListener('scroll', changeNavbar);
+
   return (
-    <div className='AboutHead'>
-      <div className="container">
-        {/* <h2>Оказание услуг по качественному медицинскому туризму граждан СНГ с ведущими специалистами г. Ташкента.</h2> */}
-      </div>
+    <div className={`AboutHead ${navbar ? 'AboutHeadActive' : ''}`}>
+
     </div>
   )
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -5,6 +5,8 @@ export default function About() {
   return (
     <div className='About'>
       <AboutHead/>
+      <h2>Оказание услуг по качественному медицинскому туризму граждан СНГ с ведущими специалистами г. Ташкента.</h2>
+      
     </div>
   )
 }
